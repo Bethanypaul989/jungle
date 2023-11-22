d0a5006b13647d46fae755c4eab280eae20ae82f
Bethanypaul989 <slayy.mayaa188@gmail.com>
2023-11-22 00:35:09 -0700

    about us page added, scss update routes etc update

--- config/routes.rb
+++ config/routes.rb
@@ -1,79 +1,28 @@
-Rails.application.routes.draw do
+require_relative "boot"
 
-  # 'products' = controller, 'index' = action
-  root to: 'products#index' # defines the '/' - the homepage - 
-  #  Ruby knows the 'parse' the special string 
-  #  get '/' => 'products#index'
-  
+require "rails/all"
 
-  resources :products, only: [:index, :show] # enpoint/pathway to /products/:id
-  resources :categories, only: [:show]
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
-  resource :cart, only: [:show] do
-    post   :add_item
-    post   :remove_item
+module New
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+
+    # Configure HTTP Basic Authentication
+    config.middleware.use(Rack::Auth::Basic) do |username, password|
+      username = ENV['ADMIN_USERNAME']
+      password = ENV['ADMIN_PASSWORD']
+    end
+    
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
-
-  resources :orders, only: [:create, :show]
-
-  namespace :admin do
-    root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
-    resources :categories, only: [:index, :new, :create, :destroy]
-  end
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
